78b844bb122e587ec291842d0741226b4209685a
AlejandroZhengZheng <ale0720199@gmail.com>
2022-04-11 22:25:06 -0400

    firebase

--- controllers/incident.js
+++ controllers/incident.js
@@ -16,199 +16,199 @@ function getErrorMessage(err) {
 
 exports.incidentList = function(req, res, next) {
 
-<<<<<<< HEAD
-    Incident.find((err, incidentList) => {
-        if (err) {
-            return console.error(err);
-        } else {
-=======
-    try {
-        // get the firebase instance of firestore
-        let db = fs.firestore();
+        HEAD
+        Incident.find((err, incidentList) => {
+                if (err) {
+                    return console.error(err);
+                } else {
 
-        // get all documents
-        let allDocs = await db.collection('inventory').get();
+                    try {
+                        // get the firebase instance of firestore
+                        let db = fs.firestore();
 
-        let docs = [];
-        allDocs.docs.map(item => {
-            docs.push(item.data());
-        })
+                        // get all documents
+                        let allDocs = await db.collection('inventory').get();
 
-        res.status(200).json(docs);
->>>>>>> parent of 371a641 (firebase_done)
+                        let docs = [];
+                        allDocs.docs.map(item => {
+                            docs.push(item.data());
+                        })
 
-            res.status(200).json(incidentList);
+                        res.status(200).json(docs);
+                        parent of 371 a641(firebase_done)
+
+                        res.status(200).json(incidentList);
+                    }
+                });
         }
-    });
-}
 
-// // Gets all incidents from the Database and renders the page to list all incidents.
-// module.exports.incidentList = function(req, res, next) {
-//     Incident.find((err, incidentList) => {
-//         // console.log(incidentList);
-//         if (err) {
-//             return console.error(err);
-//         } else {
-//             res.render('incident/list', {
-//                 title: 'Incident List',
-//                 incidents: incidentList
-//             })
-//         }
-//     });
-// }
+        // // Gets all incidents from the Database and renders the page to list all incidents.
+        // module.exports.incidentList = function(req, res, next) {
+        //     Incident.find((err, incidentList) => {
+        //         // console.log(incidentList);
+        //         if (err) {
+        //             return console.error(err);
+        //         } else {
+        //             res.render('incident/list', {
+        //                 title: 'Incident List',
+        //                 incidents: incidentList
+        //             })
+        //         }
+        //     });
+        // }
 
-// // Gets a incident by id and renders the details page.
-// module.exports.details = (req, res, next) => {
+        // // Gets a incident by id and renders the details page.
+        // module.exports.details = (req, res, next) => {
 
-//     let id = req.params.id;
+        //     let id = req.params.id;
 
-//     Incident.findById(id, (err, incidentToShow) => {
-//         if (err) {
-//             console.log(err);
-//             res.end(err);
-//         } else {
-//             //show the edit view
-//             res.render('incident/details', {
-//                 title: 'Incident Details',
-//                 incident: incidentToShow
-//             })
-//         }
-//     });
-// }
+        //     Incident.findById(id, (err, incidentToShow) => {
+        //         if (err) {
+        //             console.log(err);
+        //             res.end(err);
+        //         } else {
+        //             //show the edit view
+        //             res.render('incident/details', {
+        //                 title: 'Incident Details',
+        //                 incident: incidentToShow
+        //             })
+        //         }
+        //     });
+        // }
 
-// // Renders the Add form using the add_edit.ejs template
-// module.exports.displayAddPage = (req, res, next) => {
+        // // Renders the Add form using the add_edit.ejs template
+        // module.exports.displayAddPage = (req, res, next) => {
 
-//     let newItem = Incident();
-//     res.render('incident/add_edit', {
-//         title: 'Incident Add/Edit',
-//         incident: newItem
-//     })
+        //     let newItem = Incident();
+        //     res.render('incident/add_edit', {
+        //         title: 'Incident Add/Edit',
+        //         incident: newItem
+        //     })
 
-// }
+        // }
 
-// Processes the data submitted from the Add form to create a new incident
-module.exports.processAdd = (req, res, next) => {
+        // Processes the data submitted from the Add form to create a new incident
+        module.exports.processAdd = (req, res, next) => {
 
-    try {
-        let newItem = Incident({
-            _id: req.body.id,
-            IncidentExplanation: req.body.IncidentExplanation,
-            User: req.body.User,
-            Department: req.body.Department,
-            Solved: req.body.Solved,
+            try {
+                let newItem = Incident({
+                    _id: req.body.id,
+                    IncidentExplanation: req.body.IncidentExplanation,
+                    User: req.body.User,
+                    Department: req.body.Department,
+                    Solved: req.body.Solved,
 
-        });
+                });
 
-        Incident.create(newItem, (err, item) => {
-            if (err) {
-                console.log(err);
+                Incident.create(newItem, (err, item) => {
+                    if (err) {
+                        console.log(err);
 
+                        return res.status(400).send({
+                            success: false,
+                            message: getErrorMessage(err)
+                        });
+                    } else {
+                        console.log(item);
+                        return res.status(200).json(item);
+                    }
+                });
+            } catch (error) {
                 return res.status(400).send({
                     success: false,
-                    message: getErrorMessage(err)
-                });
-            } else {
-                console.log(item);
-                return res.status(200).json(item);
-            }
-        });
-    } catch (error) {
-        return res.status(400).send({
-            success: false,
-            message: getErrorMessage(error)
-        });
-    }
-}
-
-// // Gets a incident by id and renders the Edit form using the add_edit.ejs template
-// module.exports.displayEditPage = (req, res, next) => {
-
-//     let id = req.params.id;
-
-//     Incident.findById(id, (err, itemToEdit) => {
-//         if (err) {
-//             console.log(err);
-//             res.end(err);
-//         } else {
-//             //show the edit view
-//             res.render('incident/add_edit', {
-//                 title: 'Edit Item',
-//                 incident: itemToEdit
-//             })
-//         }
-//     });
-
-// }
-
-// Processes the data submitted from the Edit form to update a incident
-module.exports.processEditPage = (req, res, next) => {
-
-    try {
-        let id = req.params.id
-
-        let updatedItem = Incident({
-            _id: req.body.id,
-            IncidentExplanation: req.body.IncidentExplanation,
-            User: req.body.User,
-            Department: req.body.Department,
-            Solved: req.body.Solved
-        });
-
-        console.log(updatedItem);
-
-        Incident.updateOne({ _id: id }, updatedItem, (err) => {
-            if (err) {
-                console.log(err);
-
-                return res.status(400).json({
-                    success: false,
-                    message: getErrorMessage(err)
-                });
-            } else {
-
-                return res.status(200).json({
-                    success: true,
-                    message: 'Item updated successfully.'
+                    message: getErrorMessage(error)
                 });
             }
-        });
-    } catch (error) {
-        return res.status(400).send({
-            success: false,
-            message: getErrorMessage(error)
-        });
-    }
+        }
 
-}
+        // // Gets a incident by id and renders the Edit form using the add_edit.ejs template
+        // module.exports.displayEditPage = (req, res, next) => {
 
+        //     let id = req.params.id;
 
-// Deletes a incident based on its id.
-module.exports.performDelete = (req, res, next) => {
+        //     Incident.findById(id, (err, itemToEdit) => {
+        //         if (err) {
+        //             console.log(err);
+        //             res.end(err);
+        //         } else {
+        //             //show the edit view
+        //             res.render('incident/add_edit', {
+        //                 title: 'Edit Item',
+        //                 incident: itemToEdit
+        //             })
+        //         }
+        //     });
 
+        // }
 
-    try {
-        let id = req.params.id;
+        // Processes the data submitted from the Edit form to update a incident
+        module.exports.processEditPage = (req, res, next) => {
 
-        Incident.remove({ _id: id }, (err) => {
-            if (err) {
-                console.log(err);
+            try {
+                let id = req.params.id
+
+                let updatedItem = Incident({
+                    _id: req.body.id,
+                    IncidentExplanation: req.body.IncidentExplanation,
+                    User: req.body.User,
+                    Department: req.body.Department,
+                    Solved: req.body.Solved
+                });
+
+                console.log(updatedItem);
+
+                Incident.updateOne({ _id: id }, updatedItem, (err) => {
+                    if (err) {
+                        console.log(err);
+
+                        return res.status(400).json({
+                            success: false,
+                            message: getErrorMessage(err)
+                        });
+                    } else {
+
+                        return res.status(200).json({
+                            success: true,
+                            message: 'Item updated successfully.'
+                        });
+                    }
+                });
+            } catch (error) {
                 return res.status(400).send({
                     success: false,
-                    message: getErrorMessage(err)
-                });
-            } else {
-                return res.status(200).json({
-                    success: true,
-                    message: "Item removed successfully."
+                    message: getErrorMessage(error)
                 });
             }
-        });
-    } catch (error) {
-        return res.status(400).send({
-            success: false,
-            message: getErrorMessage(error)
-        });
-    }
 
-}
+        }
+
+
+        // Deletes a incident based on its id.
+        module.exports.performDelete = (req, res, next) => {
+
+
+            try {
+                let id = req.params.id;
+
+                Incident.remove({ _id: id }, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return res.status(400).send({
+                            success: false,
+                            message: getErrorMessage(err)
+                        });
+                    } else {
+                        return res.status(200).json({
+                            success: true,
+                            message: "Item removed successfully."
+                        });
+                    }
+                });
+            } catch (error) {
+                return res.status(400).send({
+                    success: false,
+                    message: getErrorMessage(error)
+                });
+            }
+
+        }
